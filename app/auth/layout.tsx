ba827c6587a90908c83086ff12eea4669b3ed7a2
frantanius <frantaniussinulingga@gmail.com>
2024-10-08 10:27:48 +0700

    initialize first config

---
+++ app/auth/layout.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react";
+
+export default function AuthLayout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      {children}
+    </div>
+  );
+}
