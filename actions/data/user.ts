4815155530ada31c31faa7c879c9b820d23fbb32
frantanius <frantaniussinulingga@gmail.com>
2024-10-08 16:48:22 +0700

    Setup prisma client, create model user and implement register server action to db

---
+++ actions/data/user.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { db } from "@/lib/db";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+    return user;
+  } catch (_) {
+    return null;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findFirst({ where: { id } });
+    return user;
+  } catch (_) {
+    return null;
+  }
+};
--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "./data/user";
 
 export const registerAction = async (
   values: z.infer<typeof RegisterSchema>,
@@ -11,6 +14,31 @@ export const registerAction = async (
   if (!validatedFields.success) {
     return { error: "Invalid field!" };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  /*
+    Check existing user by an email
+  */
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  /*
+    if user not exist create new user
+  */
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: sent verification token email
 
   return { success: "Register success" };
 };
