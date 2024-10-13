a0215894540ed051e1b6528d60e556f6c14723b3
frantanius <frantaniussinulingga@gmail.com>
2024-10-13 16:15:22 +0700

    build settings page

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -9,7 +9,7 @@ export default async function ProtectedLayout({ children }: PropsWithChildren) {
   return (
     <SessionProvider session={session}>
       <Toaster />
-      <div className="flex h-full w-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="flex h-full w-full flex-col items-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
         <Navbar />
         {children}
       </div>
